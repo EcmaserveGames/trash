ab85e6cb9df71f4519ade4fcf7a19c32807dd5ce
Ryan Hafer <hafer.ryan@gmail.com>
2020-11-08 20:51:36 -0500

    fix: let rounds actually complete and keep incrementing down card slots 🐜

--- src/server/mechanics/discard.ts
+++ src/server/mechanics/discard.ts
@@ -1,56 +1,68 @@
-import { Mechanic } from "@ecmaservegames/host";
-import { dealCard, shuffleFullDeck, tableauToSpotArray } from "../helpers";
-import { IActions, IState, UserContext } from "../types";
+import { Mechanic } from '@ecmaservegames/host'
+import { dealCard, shuffleFullDeck, tableauToSpotArray } from '../helpers'
+import { IActions, ICardSpot, IState, UserContext } from '../types'
+
+const isNotTrash = (cardSpot: ICardSpot) =>
+  !cardSpot.isRequired || !cardSpot.faceDown
 
 export const Discard = new Mechanic<IActions, IState, UserContext>('discard')
-    .forActions('discard')
-    .use(({ gameState, user }) => {
-        gameState.tableaus = gameState.tableaus || [];
-        const tableau = gameState.tableaus?.find(x => x.playerId === user.sub);
-        if (!tableau || !tableau.cardInHand) return;
+  .forActions('discard')
+  .use(({ gameState, user }) => {
+    gameState.tableaus = gameState.tableaus || []
+    const tableau = gameState.tableaus?.find((x) => x.playerId === user.sub)
+    if (!tableau || !tableau.cardInHand) return
 
-        const card = tableau.cardInHand;
-        tableau.cardInHand = null;
+    const card = tableau.cardInHand
+    tableau.cardInHand = null
 
-        // Add to the discard pile
-        gameState.discardPile = gameState.discardPile || { cards: [] };
-        gameState.discardPile.cards = gameState.discardPile?.cards || [];
-        gameState.discardPile.cards.unshift(card)
+    // Add to the discard pile
+    gameState.discardPile = gameState.discardPile || { cards: [] }
+    gameState.discardPile.cards = gameState.discardPile?.cards || []
+    gameState.discardPile.cards.unshift(card)
 
-        // Next Players Turn
-        const index = gameState.tableaus?.indexOf(tableau);
-        const nextPlayerTableau = gameState.tableaus[index + 1] || gameState.tableaus[0]
-        gameState.currentPlayerId = nextPlayerTableau.playerId
+    // Next Players Turn
+    const index = gameState.tableaus?.indexOf(tableau)
+    const nextPlayerTableau =
+      gameState.tableaus[index + 1] || gameState.tableaus[0]
+    gameState.currentPlayerId = nextPlayerTableau.playerId
 
-        // Check if the current player has an empty tableau
-        const roundOver = tableauToSpotArray(nextPlayerTableau).every(t => t.isRequired && !t.faceDown);
-        
-        // Finalize Round
-        if (roundOver) {
-            // Iterate and drop the last required slot if player has all thery cards face  up
-            for(let tableau of gameState.tableaus) {
-                const slots = tableauToSpotArray(tableau).reverse();
-                const wentOut = slots.every(t => t.isRequired && !t.faceDown);
-                if (wentOut) {
-                    const lastSlot = slots.find(x => x.isRequired);
-                    if (!lastSlot) {
-                        // I THINK THIS IS GAME OVER?
-                        return;
-                    }
-                    lastSlot.isRequired = true;
-                    lastSlot.current = null;
-                }
-            }
-            // Redeal all cards
-            const startingDeck = shuffleFullDeck(gameState);
-            gameState.tableaus?.forEach(tableau => {
-                tableauToSpotArray(tableau)
-                    .filter(x => x.isRequired)
-                    .forEach(slot => {
-                        const card = startingDeck.shift();
-                        if (!slot || !card) return;
-                        dealCard(card, slot);
-                    })
-            })
+    // Check if the current player has an empty tableau
+    const roundOver = tableauToSpotArray(nextPlayerTableau).every(isNotTrash)
+
+    // Finalize Round
+    if (roundOver) {
+      // Iterate and drop the last required slot if player has all thery cards face  up
+      for (let tableau of gameState.tableaus) {
+        const slots = tableauToSpotArray(tableau).reverse()
+        const wentOut = slots.every(isNotTrash)
+        if (wentOut) {
+          const lastSlot = slots.find((x) => x.isRequired)
+          if (!lastSlot) {
+            // I THINK THIS IS GAME OVER?
+            return
+          }
+          lastSlot.isRequired = false
+          lastSlot.current = null
         }
-    })
+      }
+      // Redeal all cards
+      const startingDeck = shuffleFullDeck(gameState)
+      gameState.tableaus?.forEach((tableau) => {
+        tableauToSpotArray(tableau)
+          .filter((x) => x.isRequired)
+          .forEach((slot) => {
+            const card = startingDeck.shift()
+            if (!slot || !card) return
+            dealCard(card, slot)
+          })
+      })
+      gameState.discardPile = { cards: [] }
+      const startingDiscardCard = startingDeck.shift()
+      if (startingDiscardCard) {
+        gameState.discardPile?.cards?.push(startingDiscardCard)
+      }
+      gameState.drawPile = {
+        cards: startingDeck,
+      }
+    }
+  })
